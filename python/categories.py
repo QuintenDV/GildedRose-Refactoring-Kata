f71fc041222a9050c46ecbc43cf103977d2dc010
QuintenDV <quintendevos3@hotmail.com>
2020-09-12 11:27:09 +0200

    Cleaned the update_quality functions and abstracted some duplicate code by moving it to the Category class

--- python/categories.py
+++ python/categories.py
@@ -8,19 +8,27 @@ class Category:
     def get_item_category(self, item):
         return self.special_items.get(item.name, General)
 
+    @classmethod
+    def increase_quality(cls, item, amount_to_increase=1):
+        item.quality += amount_to_increase
+        item.quality = min(item.quality, 50)
+
+    @classmethod
+    def decrease_quality(cls, item, amount_to_decrease=1):
+        item.quality -= amount_to_decrease
+        item.quality = max(item.quality, 0)
+
+    @classmethod
+    def decrease_sell_in(cls, item):
+        item.sell_in -= 1
 
 class General(Category):
     @classmethod
     def update_quality(cls,item):
-        if item.quality > 0:
-            item.quality = item.quality - 1
-        else:
-            if item.quality < 50:
-                item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
+        cls.decrease_sell_in(item)
         if item.sell_in < 0:
-            if item.quality > 0:
-                item.quality = item.quality - 1
+            cls.decrease_quality(item)
+        cls.decrease_quality(item)
 
 class Legendary(Category):
     @classmethod
@@ -30,24 +38,26 @@ class Legendary(Category):
 class AgesWell(Category):
     @classmethod
     def update_quality(cls, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
+        cls.decrease_sell_in(item)
         if item.sell_in < 0:
-            if item.quality < 50:
-                item.quality = item.quality + 1
+            cls.increase_quality(item)
+        cls.increase_quality(item)
 
 class BackstagePass(Category):
     @classmethod
     def update_quality(cls, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if item.sell_in < 11:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-            if item.sell_in < 6:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-        item.sell_in = item.sell_in - 1
+        amount_to_increase = cls.calculate_amount_to_increase(item)
+        cls.increase_quality(item, amount_to_increase)
+
+        cls.decrease_sell_in(item)
         if item.sell_in < 0:
             item.quality = 0
+
+    @classmethod
+    def calculate_amount_to_increase(cls, item):
+        amount_to_increase = 1
+        if item.sell_in < 11:
+            amount_to_increase += 1
+        if item.sell_in < 6:
+            amount_to_increase += 1
+        return amount_to_increase
